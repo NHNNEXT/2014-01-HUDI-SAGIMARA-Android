ddd3c5bea80a7a0abbcb4de65617b959da6441f4
sunghwan <sunghwanjo@nhnnext.org>
2014-04-07 17:38:09 +0900

    add ServerProxy & add Permission about Internet on AndroidManifest

---
+++ src/org/nhnnext/sagimara/ServerLoadProxy.java
@@ -0,0 +1,12 @@
+package org.nhnnext.sagimara;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ServerLoadProxy {
+
+	
+	
+}
